127e1496bc286a570d950a3251921d9cf977bb75
doeni <jandominicalica03@gmail.com>
2026-05-22 13:23:09 +1200

    added more code layout for later

--- hotspotscript.js
+++ hotspotscript.js
@@ -1,34 +1,39 @@
+const params = new URLSearchParams(window.location.search);
+
+const startScene = params.get("scene") || "scene1";
+
 pannellum.viewer('viewer', {
     "default": {
-        "firstScene": "scene1",
-        "sceneFadeDuration": 1000
+        "firstScene": startScene,
+        "sceneFadeDuration": 2000
     },
     "scenes": {
-        "scene1": {
+        "hotSpotDebug": true,
+        "scene1": { // sceneId
             "title": "first image",
             "hfov": 110,
-            "pitch": -3,
-            "yaw": 117,
+            "pitch": 0, // starting position of viewer 0, 0 for centre of image
+            "yaw": 0,
             "type": "equirectangular",
-            "panorama": "assets/360images/blendedPanorama.png",
+            "panorama": "assets/360images/centeredPanorama.jpg",
             "hotSpots": [
                 {
-                    "pitch": 5,
-                    "yaw": -30,
+                    "pitch": 0,
+                    "yaw": 0,
                     "type": "scene",
-                    "text": "Go to the Kitchen",
+                    "text": "next scene",
                     "sceneId": "scene2" // next scene label
                 }
             ]
         },
-        "scene2": {
+        "scene2" : {  
             "title": "second image",
             "hfov": 110,
-            "pitch": 0,
-            "yaw": 180,
+            "pitch": 0, //whatever the hotspot pitch and yaw is to the prior image
+            "yaw": 0,
             "type": "equirectangular",
-            "panorama": "assets/360images/blendedPanorama2.jpg",
-            "hotSpots": [] // next room refer to line 14
-        }
+            "panorama": "assets/360images/centeredPanorama2.jpg",
+            "hotSpots": [] // next room refer to line 19
+        },
     }
 });

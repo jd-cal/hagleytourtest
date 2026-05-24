cf91baee3a26e27c3f5800432008962f282197a9
doeni <jandominicalica03@gmail.com>
2026-05-25 11:02:28 +1200

    code updates and new functions

--- hotspotscript.js
+++ hotspotscript.js
@@ -1,8 +1,7 @@
+// js script for panorama navigation like google maps through hotspot buttons might redo later with clickable floors instead
 const params = new URLSearchParams(window.location.search);
-
 const startScene = params.get("scene") || "scene1";
-
-pannellum.viewer('viewer', {
+const viewer = pannellum.viewer('viewer', {
     "default": {
         "firstScene": startScene,
         "sceneFadeDuration": 2000
@@ -18,22 +17,36 @@ pannellum.viewer('viewer', {
             "panorama": "assets/360images/centeredPanorama.jpg",
             "hotSpots": [
                 {
-                    "pitch": 0,
+                    "pitch": 0, //position of the hotspot next scene button
                     "yaw": 0,
                     "type": "scene",
-                    "text": "next scene",
-                    "sceneId": "scene2" // next scene label
+                    "text": "walk to next scene", 
+                    "clickHandlerFunc": changeScene,
+                    "clickHandlerArgs": {
+                        "scene": "scene2"// next scene label
+                    }
                 }
             ]
         },
-        "scene2" : {  
+        "scene2" : {
             "title": "second image",
             "hfov": 110,
-            "pitch": 0, //whatever the hotspot pitch and yaw is to the prior image
-            "yaw": 0,
+            "pitch": 10, //the pitch and yaw of the scene when selected from map.html instead of through the hotspot
+            "yaw": 50,
             "type": "equirectangular",
             "panorama": "assets/360images/centeredPanorama2.jpg",
-            "hotSpots": [] // next room refer to line 19
-        },
+            "hotSpots": [] // next room refer to line 20 for reference
+        }
     }
 });
+
+//function to record the current pitch and yaw and apply it to the next scene
+function changeScene(event, args) {
+    const currentPitch = viewer.getPitch();
+    const currentYaw = viewer.getYaw();
+    viewer.loadScene(
+        args.scene,
+        currentPitch,
+        currentYaw
+    );
+}
